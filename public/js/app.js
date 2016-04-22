ca8a19bc1db2e85047a9e9b2905b5d9274403a63
Robert Rees <shudderfix@gmail.com>
2016-04-22 08:42:30 +0100

    Creates and registers the service workers

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
 console.log('Hello world');
+
+if('serviceWorker' in navigator) {
+	console.log('Service worker present');
+
+	navigator.serviceWorker.register('/js/sw.js').then((reg) => {
+		console.log('Registration succeeded: ' + reg.scope);
+	}).catch((error) => {
+		console.log('Registration failed: ' + error);
+	});
+}
